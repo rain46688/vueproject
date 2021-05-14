415439937e691c935d582e38ee838622761d56be
rain46688 <rain46688@gmail.com>
2021-05-14 11:40:07 +0900

    뷰뷰2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
     <div>
-        <h1>홈페이지 기본</h1>
+        <h1>홈페이지 기본 {{title}}</h1>
+        <input type="text" v-model="input1"/>
+        <button type="button" @click="getData">버튼</button>
+        <button type="button" @click="setData">버튼2</button>
     </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            title:"하이하이",
+            title2:"핼로우",
+            input1 : "ㅎㅎ",
+        };
+    },
+    methods:{
+        getData(){
+            alert(this.input1);
+        },
+        setData(){
+            this.input1 = "123123";
+        }
+    }
 }
 </script>
